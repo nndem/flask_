2435d44663220d401add6e6b06acaf2fcaff414d
Nick <demidov_nn@inbox.ru>
2020-10-17 00:32:10 +0300

    add simple route

---
+++ server.py
@@ -0,0 +1,27 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def home():
+    return 'Hello World MTF'
+
+
+@app.route('/about')
+def about():
+    return "The About Page"
+
+
+@app.route('/blog')
+def blog():
+    return 'This is the blog'
+
+
+@app.route('/blog/<string:blog_id>')
+def blogpost(blog_id):
+    return 'This is blog post number' + blog_id
+
+
+if __name__ == "__main__":
+    app.run()
